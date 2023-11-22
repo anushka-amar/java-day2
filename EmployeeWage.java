45d0e258fb5290680245d1848aa535caec509c59
Anushka <anushkamar1102@gmail.com>
2023-11-22 13:08:18 +0530

    Refractored: employee attendance checker

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,20 @@
 import java.util.Random;
 public class EmployeeWage{
+
+    static void empAtt(){
+        Random random = new Random();
+
+        int randNum = Math.abs(random.nextInt());
+        int empCheck = randNum%2;
+
+        if(empCheck == 1){
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is absent");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage calculator");
+        empAtt();
     }
 }
