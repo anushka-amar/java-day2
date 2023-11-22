5b1ad288456532c9d7669f20cc05dd2a3a7a0d57
Anushka <anushkamar1102@gmail.com>
2023-11-22 13:21:33 +0530

    Refractored: calculating daily wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,7 @@
 import java.util.Random;
-public class EmployeeWage{
+import java.util.Scanner;
+
+public class EmployeeWage {
 
     static void empAtt(){
         Random random = new Random();
@@ -13,8 +15,19 @@ public class EmployeeWage{
             System.out.println("Employee is absent");
         }
     }
+
+    static void dailyEmpWage(int workHr){
+        int wagePerHr = 20;
+
+        System.out.println("Daily Employee wage is "+ workHr*20);
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to employee wage calculator");
         empAtt();
+
+        System.out.println("Enter number of hours worked");
+        int daily_workHr = scanner.nextInt();
+        dailyEmpWage(daily_workHr);
     }
 }
