aea5c87e4b60f8d2f54883d2af6ad10142e19255
Anushka <anushkamar1102@gmail.com>
2023-11-22 19:32:46 +0530

    Refractor: Checking equality of lines using .equals funcn

--- LineComputation.java
+++ LineComputation.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class LineComputation {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Line Computation Comparison Program");
 
+    static double calculate_length(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the coordinates of first point P(x1,y1)");
         double x1 = scanner.nextDouble();
         double y1 = scanner.nextDouble();
 
-        System.out.println("Enter the coordinates of first point Q(x2,y2)");
+        System.out.println("Enter the coordinates of second point Q(x2,y2)");
         double x2 = scanner.nextDouble();
         double y2 = scanner.nextDouble();
 
@@ -18,5 +17,32 @@ public class LineComputation {
         double length = Math.sqrt(t1*t1 + t2*t2);
 
         System.out.println("The length of your line is "+length);
+
+        return length;
+
+    }
+
+    static void check_equality(double line1, double line2){
+        String l1 = Double.toString(line1);
+        String l2 = Double.toString(line2);
+
+        boolean equality = l1.equals(l2);
+
+        if(equality){
+            System.out.println("The lines are equal");
+        }else {
+            System.out.println("The lines are unequal");
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Computation Comparison Program");
+
+        double line1 = calculate_length();
+        double line2 = calculate_length();
+
+        check_equality(line1, line2);
+
+
     }
 }
