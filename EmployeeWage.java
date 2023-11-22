51161abddd9a5884080a03e448f5005efccbdaa1
Anushka <anushkamar1102@gmail.com>
2023-11-22 13:33:43 +0530

    Refractored: calculating wage if the emp is part-time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -18,16 +18,40 @@ public class EmployeeWage {
 
     static void dailyEmpWage(int workHr){
         int wagePerHr = 20;
+        System.out.println("Daily Employee wage is "+ workHr*wagePerHr);
+    }
 
-        System.out.println("Daily Employee wage is "+ workHr*20);
+    static void partWage(int workHr){
+        String partEmp = "Jane doe";
+        int wagePerHr = 12;
+        System.out.println("The part time employee is "+partEmp+" and her daily wage is "+ wagePerHr*workHr);
+
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to employee wage calculator");
+
         empAtt();
 
-        System.out.println("Enter number of hours worked");
-        int daily_workHr = scanner.nextInt();
-        dailyEmpWage(daily_workHr);
+
+        dailyEmpWage(8);
+
+        System.out.println("Enter if you are a part-time employee or full-time employee");
+        String emp_type = scanner.nextLine();
+        switch (emp_type){
+            case "part":
+                System.out.println("Enter number of hours worked");
+                int work_hr = scanner.nextInt();
+                partWage(work_hr);
+                break;
+
+            case "full":
+                dailyEmpWage(8);
+                break;
+
+            default:
+                System.out.println("INVALID");
+                break;
+        }
     }
 }
